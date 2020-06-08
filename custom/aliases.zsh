fc51519a9292d68ceee8dcdb0c773af3f57724b1
Ram Goli <ramsgoli@gmail.com>
2020-06-07 23:35:59 -0700

    add aliases

---
+++ custom/aliases.zsh
@@ -0,0 +1,13 @@
+# git
+alias gs="git status"
+alias gch="git checkout"
+
+# neovim
+alias n="nvim"
+
+# tmux
+alias t="tmux"
+alias tl="tmux ls"
+alias tn="tmux new-session -s"
+alias ta="tmux attach-session -t"
+alias tk="tmux kill-session -t"
