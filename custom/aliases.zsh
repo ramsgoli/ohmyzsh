dd581d2bad4c1d4aa62e0165860c2877e11d12b5
Ram Goli <ramsgoli@gmail.com>
2022-10-10 17:14:08 -0700

    add more aliases

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -2,6 +2,7 @@
 alias gs="git status"
 alias gch="git checkout"
 alias gcb="git branch | fzf | xargs git checkout"
+alias gdb="git branch | fzf -m | xargs git branch -D"
 
 # neovim
 alias n="nvim"
@@ -16,3 +17,7 @@ alias tk="tmux kill-session -t"
 # ranger
 disable r
 alias r="ranger"
+
+# util
+alias cdheap="cd ~/heap/heap"
+alias cdqe="cdheap && cd context/event_store/query_engine"
